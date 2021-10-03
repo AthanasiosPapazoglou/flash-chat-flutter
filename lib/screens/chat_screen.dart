8bc312a88d0f1937d888705999d1fcd5abd732ba
Athanasios Papapzoglou <athanasiospap95@gmail.com>
2021-10-03 21:48:54 +0300

    basic stream implementation

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -35,6 +35,22 @@ class _ChatScreenState extends State<ChatScreen> {
         print(e);
       }
   }
+
+/*
+  void getMessages() async {
+    final messages = await _firestore.collection('messages').get();
+    for (var message in messages.docs) {
+      print(message.data);
+    }
+  }*/
+
+  void messagesStream() async{
+   await for( var snapshot in _firestore.collection('messages').snapshots()){
+     for( var message in snapshot.docs){
+       print(message.data());
+     }
+   }
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -45,9 +61,7 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                _auth.signOut();
-                Navigator.pop(context);
-                //Implement logout functionality
+                messagesStream();
               }),
         ],
         title: Text('⚡️Chat'),
